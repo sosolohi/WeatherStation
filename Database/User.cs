c7e9fe24a07971756fa5253e7b5934ecb59bf59d
uczen <uczen@STANISLAW>
2025-11-14 14:59:25 +0100

    asd

---
+++ Database/User.cs
@@ -0,0 +1,8 @@
+﻿namespace Database
+{
+    public class User
+    {   
+        public int Id { get; set; }
+        string Name { get; set; }
+    }
+}
---
+++ WeatherStation/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Database;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        User user = new User();
+    }
+}
+
