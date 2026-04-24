1979e93eb50bc2276fc00de226eba698906b26e0
uczen <uczen@STANISLAW>
2026-04-24 15:42:39 +0200

    UnitTests added

---
+++ UnitTests/UserRepositoryTests/GetUserTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.UserRepositoryTests
+{
+  
+    public class GetUserTests
+    {
+        [Fact]
+        public void GetUser_ReturnsUserWithSuccess_WhenUserExistsInDatabase()
+        {
+            
+        }
+        [Fact]
+        public void GetUser_ReturnNull_WhenUserDoesNotExistInDatabase()
+        {
+
+        }
+    }
+}
